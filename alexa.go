ae14edd61098cbe2ce47339de55f3871545de177
Monica Chew <mmc@mozilla.com>
2014-10-16 13:49:57 -0700

    Format

--- alexa.go
+++ alexa.go
@@ -5,25 +5,38 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	//"math"
 	"os"
 	"strconv"
 )
 
 // An EntriesFile represents a file containing compressed log entries.
 type AlexaRank struct {
-	init bool
-	m    map[string]int64
+	init  bool
+	m     map[string]int64
+	count int
 }
 
+// Returns a rank for the site in question, or -1 if the host doesn't exist. Lower is better.
 func (a *AlexaRank) GetRank(host string) (rank int64, err error) {
 	if a.m == nil {
-		return 0, errors.New("Alexa: Must initialize before getting rank")
+		return -1, errors.New("Alexa: Must initialize before getting rank")
 	}
 	val := a.m[host]
 	if val == 0 {
-		return 0, errors.New("Alexa: No rank for host")
+		return -1, errors.New("Alexa: No rank for host")
 	}
 	return val, nil
+}
+
+// Returns a reputation between [0, 1] or -1 if the host does not exist in the ranking.
+func (a *AlexaRank) GetReputation(host string) (reputation float32, err error) {
+	rank, err := a.GetRank(host)
+	if err != nil {
+		return -1, err
+	}
+	// Rank starts at 1, normalize to 0 so the top-ranked site can have reputation = 1.0
+	return 1.0 - float32(rank-1)/float32(a.count), nil
 }
 
 func (a *AlexaRank) Init(fileName string) {
@@ -48,4 +61,5 @@ func (a *AlexaRank) Init(fileName string) {
 		rank, _ := strconv.ParseInt(record[0], 0, 0)
 		a.m[record[1]] = rank
 	}
+	a.count = len(a.m)
 }
--- alexa_test.go
+++ alexa_test.go
@@ -12,4 +12,22 @@ func TestInit(t *testing.T) {
 	if rank != 1 {
 		t.Error("Expected rank 1 for google.com: %d", rank)
 	}
+	rank, err = a.GetRank("example.com")
+	if err == nil {
+		t.Errorf("Expected error for rank of example.com")
+	}
+	if rank != -1 {
+		t.Errorf("Expected -1 for rank of example.com")
+	}
+	rep, err := a.GetReputation("taobao.com")
+	if err != nil {
+		t.Errorf("Expected to find reputation for taobao: %s", err)
+	}
+	if (rep - 0.1) > 0.0000001 {
+		t.Errorf("Expected reputation 0.1 for taobao: %f", rep)
+	}
+	rep, err = a.GetReputation("google.com")
+	if rep != 1.0 {
+		t.Errorf("Expected reputation 1.0 for google: %f", rep)
+	}
 }
